9561e843b82a96e04484d70813d3fe643c2e80a4
Andrew Janzen <a.s.janzen@gmail.com>
2018-11-30 10:53:06 -0800

    scraper auto-scrolls page to deal with infinite scroll

--- rd/index.js
+++ rd/index.js
@@ -5,31 +5,36 @@ const sleep = ms => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// const downloadAll = async () => {
-//   const imgs = await scrapeImgUrls();
+const autoScroll = async page => {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      const distance = 100;
+      let totalHeight = 0;
+      let timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
 
-//   await Promise.all(imgs.map(async (file) => {
-//     await downloadImg({
-//       url: file,
-//       dest: '../../../../Pictures/covers'
-//     });
-//   }));
-//   console.log("Done.");
-// };
-
-
-scrapeImgUrls = async query => {
-  // const browser = await puppeteer.launch({
-  //   headless: false,
-  //   defaultViewport: { width: 1024, height: 768 }
-  // });
-  const browser = await puppeteer.launch();
+const scrapeImgUrls = async query => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: { width: 1024, height: 768 }
+  });
+  // const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(`https://www.google.com/search?&tbm=isch&q=${query}`);
+  await autoScroll(page);
   const results = await page.evaluate(() => {
     const imageLinks = [];
     const elements = document.querySelectorAll('img.rg_ic.rg_i');
-
     for (const element of elements) {
       if (element.src) {
         imageLinks.push(element.src);
@@ -39,11 +44,6 @@ scrapeImgUrls = async query => {
     return imageLinks;
   });
 
-  // for (let i = 0; i < results.length; i++) {
-  //   await page.goto(results[i]);
-  //   await sleep(1000);
-  // }
-
   browser.close();
   return results;
 };
@@ -52,250 +52,17 @@ scrapeImgUrls = async query => {
   prompt.start();
   await prompt.get(['search'], async (error, result) => {
     const urls = await scrapeImgUrls(result.search);
-    // const urls = await scrapeImgUrls('cat');
-
     const browser = await puppeteer.launch({
       headless: false,
       defaultViewport: { width: 1024, height: 768 }
     });
-    const page = await browser.newPage();
 
+    const page = await browser.newPage();
     for (let i = 0; i < urls.length; i++) {
       await page.goto(urls[i]);
       await sleep(1000);
     }
 
-    console.log(urls.length);
-
-    // browser.close();
+    browser.close();
   });
 })();
-
-
-
-// (async () => {
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     defaultViewport: { width: 1024, height: 768 }
-//   });
-//   const page = await browser.newPage();
-//   const searchQuery = 'see food octopus';
-//   await page.goto(`https://www.google.com/search?&tbm=isch&q=${searchQuery}`);
-//   const results = await page.evaluate(() => {
-//     const imageLinks = [];
-//     const elements = document.querySelectorAll('img.rg_ic.rg_i');
-
-//     for (const element of elements) {
-//       if (element.src) {
-//         imageLinks.push(element.src);
-//       }
-//     }
-
-//     return imageLinks;
-//   });
-
-//   // for (let i = 0; i < results.length; i++) {
-//   //   await page.goto(results[i]);
-//   //   await sleep(1000);
-//   // }
-// })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const puppeteer = require('puppeteer');
-// const prompt = require('prompt');
-
-// const sleep = ms => {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
-// // const downloadAll = async () => {
-// //   const imgs = await scrapeImgUrls();
-
-// //   await Promise.all(imgs.map(async (file) => {
-// //     await downloadImg({
-// //       url: file,
-// //       dest: '../../../../Pictures/covers'
-// //     });
-// //   }));
-// //   console.log("Done.");
-// // };
-
-
-// scrapeImgUrls = async query => {
-//   // const browser = await puppeteer.launch({
-//   //   headless: false,
-//   //   defaultViewport: { width: 1024, height: 768 }
-//   // });
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto(`https://www.google.com/search?&tbm=isch&q=${query}`);
-//   const results = await page.evaluate(() => {
-//     const imageLinks = [];
-//     const elements = document.querySelectorAll('img.rg_ic.rg_i');
-
-//     for (const element of elements) {
-//       if (element.src) {
-//         imageLinks.push(element.src);
-//       }
-//     }
-
-//     return imageLinks;
-//   });
-
-//   // for (let i = 0; i < results.length; i++) {
-//   //   await page.goto(results[i]);
-//   //   await sleep(1000);
-//   // }
-
-//   browser.close();
-//   return results;
-// };
-
-// (async () => {
-//   prompt.start();
-//   await prompt.get(['search'], async (error, result) => {
-//     const urls = await scrapeImgUrls(result.search);
-//     // const urls = await scrapeImgUrls('cat');
-
-//     const browser = await puppeteer.launch({
-//       headless: false,
-//       defaultViewport: { width: 1024, height: 768 }
-//     });
-//     const page = await browser.newPage();
-
-//     for (let i = 0; i < urls.length; i++) {
-//       await page.goto(urls[i]);
-//       await sleep(1000);
-//     }
-
-//     console.log(urls.length);
-
-//     // browser.close();
-//   });
-// })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // funny, try typing cat
-// const puppeteer = require('puppeteer');
-// const prompt = require('prompt');
-
-// const sleep = ms => {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
-// // const downloadAll = async () => {
-// //   const imgs = await scrapeImgUrls();
-
-// //   await Promise.all(imgs.map(async (file) => {
-// //     await downloadImg({
-// //       url: file,
-// //       dest: '../../../../Pictures/covers'
-// //     });
-// //   }));
-// //   console.log("Done.");
-// // };
-
-
-// scrapeImgUrls = async query => {
-//   // const browser = await puppeteer.launch({
-//   //   headless: false,
-//   //   defaultViewport: { width: 1024, height: 768 }
-//   // });
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto(`https://www.google.com/search?&tbm=isch&q=${query}`);
-//   const results = await page.evaluate(() => {
-//     const imageLinks = [];
-//     const elements = document.querySelectorAll('img.rg_ic.rg_i');
-
-//     for (const element of elements) {
-//       if (element.src) {
-//         imageLinks.push(element.src);
-//       }
-//     }
-
-//     return imageLinks;
-//   });
-
-//   // for (let i = 0; i < results.length; i++) {
-//   //   await page.goto(results[i]);
-//   //   await sleep(1000);
-//   // }
-
-//   browser.close();
-//   return results;
-// };
-
-// (async () => {
-//   prompt.start();
-//   prompt.get(['search'], async (error, result) => {
-
-//     const urls = await scrapeImgUrls(result);
-//     // const urls = await scrapeImgUrls('cat');
-
-//     const browser = await puppeteer.launch({
-//       headless: false,
-//       defaultViewport: { width: 1024, height: 768 }
-//     });
-//     const page = await browser.newPage();
-
-//     for (let i = 0; i < urls.length; i++) {
-//       await page.goto(urls[i]);
-//       await sleep(1000);
-//     }
-//   });
-
-//   // browser.close();
-// })();
-
-
-
-// // (async () => {
-// //   const browser = await puppeteer.launch({
-// //     headless: false,
-// //     defaultViewport: { width: 1024, height: 768 }
-// //   });
-// //   const page = await browser.newPage();
-// //   const searchQuery = 'see food octopus';
-// //   await page.goto(`https://www.google.com/search?&tbm=isch&q=${searchQuery}`);
-// //   const results = await page.evaluate(() => {
-// //     const imageLinks = [];
-// //     const elements = document.querySelectorAll('img.rg_ic.rg_i');
-
-// //     for (const element of elements) {
-// //       if (element.src) {
-// //         imageLinks.push(element.src);
-// //       }
-// //     }
-
-// //     return imageLinks;
-// //   });
-
-// //   // for (let i = 0; i < results.length; i++) {
-// //   //   await page.goto(results[i]);
-// //   //   await sleep(1000);
-// //   // }
-// // })();
