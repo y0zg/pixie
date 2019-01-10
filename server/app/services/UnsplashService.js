f28afa457a3cbb1a927eff97090fa831bba7a6cf
Andrew Janzen <a.s.janzen@gmail.com>
2019-01-09 18:37:09 -0800

    move unsplash client id out of global scope

--- server/app/services/UnsplashService.js
+++ server/app/services/UnsplashService.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const client_id = process.env.UNSPLASH_ID;
-
 class UnsplashService {
   static search(query, page, per_page) {
+    const client_id = process.env.UNSPLASH_ID;
     return axios.get('https://api.unsplash.com/search/photos', {
       params: { query, page, per_page, client_id }
     });
